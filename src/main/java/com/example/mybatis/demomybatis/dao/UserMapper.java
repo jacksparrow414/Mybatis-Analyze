2f5aa8f014dca65b6c93f7fa670c837c3033ee01
Serendipity <jacksparrow414@163.com>
2019-11-20 13:33:35 +0800

    加注释

--- src/main/java/com/example/mybatis/demomybatis/dao/UserMapper.java
+++ src/main/java/com/example/mybatis/demomybatis/dao/UserMapper.java
@@ -1,13 +1,13 @@
 package com.example.mybatis.demomybatis.dao;
 
 import com.example.mybatis.demomybatis.entity.UserEntity;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author dhb
+ * 这里目前先不加@Mapper注解,等以后看Spring和Mybatis结合的时候再加，现在是纯原生
  * @date 2019/11/14 9:43
  */
-@Mapper
+
 public interface UserMapper {
 
     void addUser(UserEntity entity);
