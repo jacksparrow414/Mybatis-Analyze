500c18f1aaad40b18f2605ff592682b7f8d260c9
duhongbo <duhb@youxuepai.com>
2019-11-20 13:33:35 +0800

    加注释

--- src/test/java/com/example/mybatis/demomybatis/MybatisTests.java
+++ src/test/java/com/example/mybatis/demomybatis/MybatisTests.java
@@ -24,6 +24,14 @@ public class MybatisTests {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // 读取配置文件,将所有配置好的Mapper接口，在XMLConfigBuilder中parseConfiguration方法里的mapperElement子方法里加进去，
+        // 最终的执行方法在XMLMapperBuilder.parse()里的bindMapperForNamespace()调用，
+        // Configuration.addMapper()方法里通过MapperRegistry.addMapper()方法，
+        // 放到一个Map里面去,
+        // 该Map的Key是Mapper接口的完整路径(仅仅是接口路径而不是接口下面具体方法的路径,要注意，接口方法的具体路径会在下面动态代理那里拿到),
+        // 该Map的value是构造一个新的MapperProxyFactory,
+        // MapperProxyFactory以便于后面调用sqlSession.getMapper(Mapper接口.class)可以通过该MapperProxyFactory生成当前Mapper接口的动态代理类MapperProxy，
+        // 具体可参考MapperRegistry.getMapper()方法即可，里面就是一个new了一个Mapper接口的动态代理对象 MapperProxy
         SqlSessionFactory factory = builder.build(inputStream);
         SqlSession sqlSession = factory.openSession();
         UserEntity entity = new UserEntity();
@@ -32,9 +40,11 @@ public class MybatisTests {
         entity.setId(7);
         // 第一种方式、获取对应的mapper，然后调用对应的方法即可,这里获取的UserMapper 的动态代理类MapperProxy对象
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        // 执行的时候调用MapperProxy.invoke方法执行
+        // 执行的时候调用MapperProxy.invoke方法执行,里面最后会执行 mapperMethod.execute(sqlSession, args)，
+        // 然后MapperMethod.execute(),根据不同的SQLCommand(insert,update,delete,select)执行sqlSession.insert等不同的方法，
+        // 接下来的执行就和第二种方式一毛一样了
         mapper.addUser(entity);
-        // 第二种方式、直接调用sqlsession的内置方法,两个参数，第一个参数是，mapper里的方法的完整路径，第二个参数是方法的入参
+        // 第二种方式、直接调用sqlsession的内置方法,两个参数，第一个参数是，mapper里的具体方法的完整路径，第二个参数是方法的入参
 //        sqlSession.insert("com.example.mybatis.demomybatis.dao.UserMapper.addUser",entity);
         sqlSession.commit();
         sqlSession.close();
