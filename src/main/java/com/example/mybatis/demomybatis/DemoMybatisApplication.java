1517d6974b2f468019c74dfda14d707bec612c46
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2019-11-23 11:08:15 +0800

    完善test例子和注释

--- src/main/java/com/example/mybatis/demomybatis/DemoMybatisApplication.java
+++ src/main/java/com/example/mybatis/demomybatis/DemoMybatisApplication.java
@@ -2,8 +2,10 @@ package com.example.mybatis.demomybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 // @MapperScan(basePackages = "com.example.mybatis.demomybatis.dao")
 public class DemoMybatisApplication {
 
