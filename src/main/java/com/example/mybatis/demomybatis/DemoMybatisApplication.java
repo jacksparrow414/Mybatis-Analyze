7dbd7ec449cc2b1123b753e7ba6c3f90060abd67
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-05-26 22:07:25 +0800

    fix(application.yml): pom文件修改、配置文件修改

--- src/main/java/com/example/mybatis/demomybatis/DemoMybatisApplication.java
+++ src/main/java/com/example/mybatis/demomybatis/DemoMybatisApplication.java
@@ -2,15 +2,14 @@ package com.example.mybatis.demomybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 如果测试是事务的方法，com.example.mybatis.demomybatis.controller.UserController#testTransactional()，
  * 直接把exclude去掉就可以，否则可能启动不起来
- *
+ * exclude = {DataSourceAutoConfiguration.class}
  * 如果使用spring-mybatis，把MapperScan注释放开
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 //@MapperScan(basePackages = "com.example.mybatis.demomybatis.dao")
 public class DemoMybatisApplication {
 
