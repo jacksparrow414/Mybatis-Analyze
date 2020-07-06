f56b07d99905ac65698c626c26ccbfbbc59fd4d6
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-07-06 19:04:24 +0800

    add new query method

--- src/main/java/com/example/mybatis/demomybatis/service/impl/UserItemServiceImpl.java
+++ src/main/java/com/example/mybatis/demomybatis/service/impl/UserItemServiceImpl.java
@@ -4,13 +4,24 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.mybatis.demomybatis.dao.UserItemMapper;
 import com.example.mybatis.demomybatis.entity.UserItem;
 import com.example.mybatis.demomybatis.service.UserItemService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
+ * serviceImpl.
  * @author jacksparrow414
  * @date 2020-05-24
  * @description: TODO
  */
 @Service
 public class UserItemServiceImpl extends ServiceImpl<UserItemMapper, UserItem> implements UserItemService {
+    
+    @Autowired
+    private UserItemMapper userItemMapper;
+    
+    @Override
+    public List<UserItem> listByCondition(Integer age) {
+        return userItemMapper.listByCondition(age);
+    }
 }
