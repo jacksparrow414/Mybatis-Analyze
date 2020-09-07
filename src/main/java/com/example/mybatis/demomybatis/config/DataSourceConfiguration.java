042bba2a1e90ce3a4f8c1e17940744be9ef0bf64
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-09-07 22:08:45 +0800

    自定义DataSource配置类加注释

--- src/main/java/com/example/mybatis/demomybatis/config/DataSourceConfiguration.java
+++ src/main/java/com/example/mybatis/demomybatis/config/DataSourceConfiguration.java
@@ -26,7 +26,11 @@ import org.springframework.core.env.Environment;
  * <p>模仿ShardingSphere配置数据源的实现思路</p>
  *
  * <p>使用{@link org.springframework.boot.context.properties.bind.Binder}
- * 获取具体属性下的集合的值,需要注入Environment</p>
+ * 获取【配置文件具体属性】下的集合的值,需要注入Environment，也可以实现{@link org.springframework.context.EnvironmentAware}接口，
+ * 在具体方法里处理，这个方法会有environment。【可以获取具体的属性】</p>
+ *
+ * <p>Binder和{@link org.springframework.boot.context.properties.ConfigurationProperties}很类似，
+ * 不过binder用的情况基本是我们自己手动处理【配置文件】的参数绑定</p>
  */
 @Configuration
 @AutoConfigureBefore(value = {DataSourceAutoConfiguration.class})
