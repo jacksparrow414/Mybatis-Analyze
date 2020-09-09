836f15a3e28f6e7ced192bc5c9a95d7759080272
Serendipity <jacksparrow414@163.com>
2020-09-09 17:32:40 +0800

    添加JdbcTemplate测试方法

---
+++ src/test/java/com/example/mybatis/demomybatis/jdbc/CommonJdbcTemplateTest.java
@@ -0,0 +1,18 @@
+package com.example.mybatis.demomybatis.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @author duhongbo
+ * @date 2020/9/9 17:27
+ */
+public final class CommonJdbcTemplateTest {
+    
+    private JdbcTemplate jdbcTemplate;
+    
+    @Test
+    public void assertJdbcTemplateInsert() {
+    
+    }
+}
