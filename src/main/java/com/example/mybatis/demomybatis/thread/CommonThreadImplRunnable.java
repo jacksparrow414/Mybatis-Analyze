307bc312d8ad2e500dba120277c5047a59c657fb
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-10-13 22:22:55 +0800

    修改多线程并发例子

--- src/main/java/com/example/mybatis/demomybatis/thread/CommonThreadImplRunnable.java
+++ src/main/java/com/example/mybatis/demomybatis/thread/CommonThreadImplRunnable.java
@@ -1,6 +1,8 @@
 package com.example.mybatis.demomybatis.thread;
 
-import lombok.AllArgsConstructor;
+import cn.hutool.core.util.RandomUtil;
+import lombok.Getter;
+import lombok.SneakyThrows;
 
 /**
  * 创建线程的两种方式
@@ -11,13 +13,31 @@ import lombok.AllArgsConstructor;
  * @author jacksparrow414
  * @date 2020/10/3
  */
-@AllArgsConstructor
 public class CommonThreadImplRunnable implements Runnable{
     
-    private ThreadEntity threadEntity;
+    /**
+     * tmp是线程不安全的
+     */
+    @Getter
+    private int tmp = 0;
     
     @Override
     public void run() {
-        System.out.println(threadEntity.getNumValue());
+        count();
+    }
+    
+    @SneakyThrows
+    private void count() {
+        while (true) {
+            // 线程休眠,模拟在加入同步方法的时候,CPU进行切换，否则该方法将由一个线程执行完毕
+             Thread.sleep(RandomUtil.randomInt(0, 50));
+            synchronized (this) {
+                if (tmp < 10) {
+                    // 线程休眠，模拟CPU切换造成的异常
+                    Thread.sleep(RandomUtil.randomInt(0, 50));
+                    System.out.println(Thread.currentThread().getName() + ":" + tmp++);
+                }
+            }
+        }
     }
 }
--- src/test/java/com/example/mybatis/demomybatis/thread/CommonThreadImplRunnableTest.java
+++ src/test/java/com/example/mybatis/demomybatis/thread/CommonThreadImplRunnableTest.java
@@ -1,17 +1,25 @@
 package com.example.mybatis.demomybatis.thread;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * 同一个实例并发
  * @author jacksparrow414
  * @date 2020/10/3
  */
 public final class CommonThreadImplRunnableTest {
     
+    private CommonThreadImplRunnable commonThreadImplRunnable;
+    
+    @Before
+    public void setUp() {
+        commonThreadImplRunnable = new CommonThreadImplRunnable();
+    }
     @Test
     public void assertThreadConcurrency() {
-        (new Thread(new CommonThreadImplRunnable(new ThreadEntity()))).start();
-        (new Thread(new CommonThreadImplRunnable(new ThreadEntity()))).start();
-        (new Thread(new CommonThreadImplRunnable(new ThreadEntity()))).start();
+        (new Thread(commonThreadImplRunnable)).start();
+        (new Thread(commonThreadImplRunnable)).start();
+        (new Thread(commonThreadImplRunnable)).start();
     }
 }
