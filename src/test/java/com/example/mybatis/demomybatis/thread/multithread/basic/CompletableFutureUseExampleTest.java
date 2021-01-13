9d9f97120061d78a421aa8fdf5ce167ac91533b1
jacksparrow414 <jacksparrow414@163.com>
2021-01-13 11:23:06 +0800

    加入子任务异常处理示例

--- src/test/java/com/example/mybatis/demomybatis/thread/multithread/basic/CompletableFutureUseExampleTest.java
+++ src/test/java/com/example/mybatis/demomybatis/thread/multithread/basic/CompletableFutureUseExampleTest.java
@@ -2,12 +2,14 @@ package com.example.mybatis.demomybatis.thread.multithread.basic;
 
 import cn.hutool.core.util.StrUtil;
 import com.example.mybatis.demomybatis.config.ThreadPoolConfiguration;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +28,7 @@ public final class CompletableFutureUseExampleTest {
     
     @Autowired
     private ThreadPoolExecutor threadPoolExecutor;
-    
+
     @Test
     public void assertCompletableFuture() {
         // 传入自定义线程池，如果不传，使用默认的线程池，应该传入自定义的线程池，方便管理
@@ -82,5 +84,71 @@ public final class CompletableFutureUseExampleTest {
         // 子线程执行完毕, 再执行主线程
         CompletableFuture.allOf(voidCompletableFutureSecond, voidCompletableFutureFirst).join();
         System.out.println("主线程");
+    }
+
+    /**
+     * 对于多个并行的子任务，如果这些子任务都没有返回值，那么当子任务执行过程中出现异常，在最终汇合处可以执行handle进行异常的处理.
+     * 但是如果多个子任务都出现异常了,只会打印一个异常.</br>
+     *
+     * <b>所以,正确的操作是要对每个子任务进行异常的处理，在每个子任务中使用handel或者</b>
+     */
+    @Test
+    public void assertExceptionNoResultFuture() {
+        CompletableFuture<Void> voidCompletableFutureFirst = CompletableFuture.runAsync(() -> {
+            int e = 1 / 0;
+        }, threadPoolExecutor);
+
+        CompletableFuture<Void> voidCompletableFutureSecond = CompletableFuture.runAsync(() -> {
+            List<Integer> list = Lists.newArrayList();
+            System.out.println(Thread.currentThread().getName());
+            list.get(1);
+        }, threadPoolExecutor);
+
+        CompletableFuture.allOf(voidCompletableFutureSecond, voidCompletableFutureFirst).handle((r, e) -> {
+            System.out.println("最终打印的异常"+e.getMessage());
+            return r;
+        }).join();
+
+        // 正确使用姿势
+        voidCompletableFutureFirst.handle((r, e) -> {
+            if (e == null) {
+                return r;
+            }else {
+                return e.getMessage();
+            }
+        });
+    }
+
+    /**
+     * 对于有返回值的子任务，可以使用exceptionally或者handle进行异常处理.
+     */
+    @Test
+    public void assertExceptionResultFuture() {
+        CompletableFuture<String> completableFutureFirst = CompletableFuture.supplyAsync(() -> {
+            return Thread.currentThread().getName();
+        }, threadPoolExecutor).exceptionally(e -> {
+            if (e == null) {
+                return "异常处理完毕";
+            }else {
+                return "没有异常";
+            }
+        });
+
+        CompletableFuture<String> completableFutureSecond = CompletableFuture.supplyAsync(() -> {
+            int e = 1/0;
+            return Thread.currentThread().getName();
+        }, threadPoolExecutor).handle((r, e) -> {
+            if (e == null) {
+                return r;
+            }else {
+                return "没有异常";
+            }
+        });
+
+        // 不能在allOf后使用exceptionally， 因为exceptionally是一个Function，要返回值的，而allOf没有返回值
+//        CompletableFuture.allOf(completableFutureSecond, completableFutureFirst).exceptionally(e -> {
+//            System.out.println("7777");
+//            return;
+//        });
     }
 }
