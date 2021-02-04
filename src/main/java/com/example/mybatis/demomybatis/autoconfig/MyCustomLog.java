64b375f11c0bdf0a1f40fcecc370331ff32f3977
Serendipity <jacksparrow414@163.com>
2021-02-04 21:46:32 +0800

    加入自定义自动配置示例代码

---
+++ src/main/java/com/example/mybatis/demomybatis/autoconfig/MyCustomLog.java
@@ -0,0 +1,12 @@
+package com.example.mybatis.demomybatis.autoconfig;
+
+/**
+ * @author jacksparrow414
+ * @date 2021/2/4
+ */
+//public class MyCustomLog implements MyAppLog {
+//    @Override
+//    public void logWhenStart() {
+//        System.out.println("custom log Impl");
+//    }
+//}
