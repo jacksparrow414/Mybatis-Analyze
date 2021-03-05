ab18a2ba68149a7e3d8be4c073d763fcb37b1488
Serendipity <jacksparrow414@163.com>
2021-03-05 16:15:08 +0800

    快速排序在中间开始

--- src/main/java/com/example/mybatis/demomybatis/algorithms/QuickSort.java
+++ src/main/java/com/example/mybatis/demomybatis/algorithms/QuickSort.java
@@ -12,16 +12,57 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class QuickSort {
-    
+
+    /**
+     * 普通快速-递归排序
+     * @param nums 待排序数组
+     */
     public void quickSort(int ...nums) {
         recursionCall(nums, 0, nums.length-1);
     }
-    
+
+    /**
+     * 普通快速-尾递归排序.
+     * @param nums 待排序数组
+     */
+    public void quickSortTail(int ...nums) {
+        tailRecursionCall(nums, 0, nums.length-1);
+    }
+
+    public void quickSortTailFromMiddle(int... nums) {
+        tailRecursionCallFromMiddle(nums, 0, nums.length -1);
+    }
+    /**
+     * 普通递归快排.
+     * 当递归深度足够大的时候，容易出现堆栈空间不足的情况
+     */
     private void recursionCall(int[] nums, int low, int high) {
         if(low < high) {
-            int pivot = partitionBigToSmall(nums, low, high);
+            int pivot = partitionSmallToBig(nums, low, high);
             recursionCall(nums, low, pivot-1);
+            // 一次递归->两个递归了
             recursionCall(nums, pivot+1, high);
+        }
+    }
+
+    /**
+     * 尾递归快排.
+     */
+    private void tailRecursionCall(int[] nums, int low, int high) {
+        // 使用while迭代,一次递归两次迭代
+        while (low < high) {
+            int pivot = partitionBigToSmall(nums, low, high);
+            tailRecursionCall(nums, low, pivot-1);
+            low = pivot + 1;
+        }
+    }
+
+    private void tailRecursionCallFromMiddle(int[] nums, int left, int right) {
+        while (left < right) {
+            int pivot = partitionSmallToBigFromMiddle(nums, left, right);
+            tailRecursionCallFromMiddle(nums, left, pivot-1 );
+//            tailRecursionCallFromMiddle(nums, pivot, right);
+            left = pivot;
         }
     }
     
@@ -48,7 +89,33 @@ public class QuickSort {
         // 当左边不小于右边时，说明，此时二者走到了一起，则该位置的index就是本次的中间值
         return low;
     }
-    
+
+    /**
+     * 固定基准：从中间值开始.
+     * 从小到大排.即比中间值小的在左边，比中间值大的在右边.
+     * 不再选取固定位置0作为中间位置，【选取中间位置的值作为枢轴】
+     */
+    private int partitionSmallToBigFromMiddle(int[] nums, int left, int right) {
+        int middle = (left+right) /2 ;
+        int pivotKey = nums[middle];
+        while (left <= right) {
+            while (nums[right] > pivotKey) {
+                right--;
+            }
+            // 条件判断为<，如果是<=，则指针会继续前移
+            while ( nums[left] < pivotKey) {
+                left++;
+            }
+
+            if (left <= right) {
+                SortUtil.swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+        log.info("本次排序结果为{}", nums);
+        return left;
+    }
     /**
      * 从大到小排.即比中间值小的在右边，比中间值大的在左边
      */
@@ -70,7 +137,7 @@ public class QuickSort {
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
         int[] nums = new int[]{50,10,90,30,70,40,80,60,20};
-        quickSort.quickSort(nums);
+        quickSort.quickSortTailFromMiddle(nums);
         log.info("快速排序的最终结果是{}", nums);
     }
 }
