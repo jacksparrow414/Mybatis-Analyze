d21f43ce509e5543b1b653184ab268f949feb13c
Serendipity <jacksparrow414@163.com>
2021-10-18 09:37:21 +0800

    add another way of rmi client

--- src/main/java/com/example/mybatis/demomybatis/java/advanced/RmiClient.java
+++ src/main/java/com/example/mybatis/demomybatis/java/advanced/RmiClient.java
@@ -3,7 +3,10 @@ package com.example.mybatis.demomybatis.java.advanced;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 /**
  * @author jacksparrow414
@@ -13,9 +16,11 @@ public class RmiClient {
     
     public static void main(String[] args) {
         try {
-            RmiServerInterface hello = (RmiServerInterface) Naming.lookup("rmi://localhost:2001/hello");
+            Registry registry = LocateRegistry.getRegistry(2001);
+            RmiServerInterface hello = (RmiServerInterface) registry.lookup("hello");
+//            RmiServerInterface hello = (RmiServerInterface) Naming.lookup("rmi://localhost:2001/hello");
             System.out.println("接收到服务器端的接口的返回信息是: " + hello.sayHello() +" "+ hello.getClass().getName());
-        } catch (RemoteException | NotBoundException | MalformedURLException e) {
+        } catch (RemoteException | NotBoundException e) {
             System.out.println("Connect Server fail"+ e.getMessage());
         }
     }
